eb273ce6182629fb4a1a100dd9e7c9a26565815a
kcheykim <kosalcheykim@gmail.com>
2021-12-26 03:10:06 -0800

    getting no error on npm start

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const helpers = require('./utils/helpers');
-const hbs = exphbs.create({ helpers });
+//const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
+//const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({});
 const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
